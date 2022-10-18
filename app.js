54ca7cb7b0de955a2ec599e3d558a61e66326d1c
skrylexx_ <alex.demeuleneire@gmail.com>
2022-10-18 17:31:43 +0200

    commit

--- app.js
+++ app.js
@@ -61,9 +61,27 @@ writter()
 
 $(document).on('ready', function () {
   $(".articles").slick({
-      dots: true,
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3
-  });
-});
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  })});
